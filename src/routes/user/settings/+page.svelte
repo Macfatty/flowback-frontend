b9053e58f46c965993f6d707c412e9b7d298783d
kattenelvis <officialkattenelvis@gmail.com>
2025-07-24 17:30:38 +0200

    0.2.16

--- src/routes/user/settings/+page.svelte
+++ src/routes/user/settings/+page.svelte
@@ -52,7 +52,7 @@
 		},
 		reports: report[] = [],
 		serverConfig: any = {},
-		version = '0.2.15';
+		version = '0.2.16';
 
 	const userUpdate = async () => {
 		const { res, json } = await fetchRequest('POST', 'user/update', {
