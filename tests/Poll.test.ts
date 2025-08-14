6254db019c81e2b35d9bdd3e3fbbaf7048c63f33
kattenelvis <officialkattenelvis@gmail.com>
2025-08-14 15:01:14 +0200

    prediction statement phase

--- tests/Poll.test.ts
+++ tests/Poll.test.ts
@@ -12,6 +12,7 @@ test('Create-Poll', async ({ page }) => {
     await expect(groupLink).toBeVisible();
     groupLink.click();
 
+    //Create a Poll
     await page.getByRole('button', { name: 'Create a post' }).click();
     await page.getByLabel('Title * 0/').click();
     await page.getByLabel('Title * 0/').fill('Test Poll');
@@ -24,32 +25,62 @@ test('Create-Poll', async ({ page }) => {
     await expect(page.getByRole('heading', { name: 'Test Poll' })).toBeVisible();
     await expect(page).toHaveURL(/\/groups\/6\/polls\/\d+$/);
 
-await page.getByRole('radio').check();
-await page.getByRole('button', { name: 'Submit' }).click();
-await expect(page.getByText('Successfully voted for area')).toBeVisible();
-await page.getByRole('button', { name: 'Cancel' }).click();
-await expect(page.getByText('Vote cancelled')).toBeVisible();
-await page.getByRole('radio').check();
-await page.getByRole('button', { name: 'Submit' }).click();
-await expect(page.getByText('Successfully voted for area')).toBeVisible();
-await page.locator('div:nth-child(3) > button').first().click();
-await page.getByRole('button', { name: 'Fast Forward' }).click();
+    // Area Phase
+    await page.getByRole('radio').check();
+    await page.getByRole('button', { name: 'Submit' }).click();
+    await expect(page.getByText('Successfully voted for area')).toBeVisible();
+    await page.getByRole('button', { name: 'Cancel' }).click();
+    await expect(page.getByText('Vote cancelled')).toBeVisible();
+    await page.getByRole('radio').check();
+    await page.getByRole('button', { name: 'Submit' }).click();
+    await expect(page.getByText('Successfully voted for area')).toBeVisible();
 
-//Proposal phase
+    await page.locator('#poll-header-multiple-choices').first().click();
+    await page.getByRole('button', { name: 'Fast Forward' }).click();
 
-await page.getByRole('button', { name: 'Add Proposal' }).click();
-await page.getByRole('textbox', { name: 'Title * 0/' }).click();
-await page.getByRole('textbox', { name: 'Title * 0/' }).fill('Proposal test');
-await page.getByLabel('Description  0/').click();
-await page.getByLabel('Description  0/').fill('Proposal Test');
-await page.getByRole('button', { name: 'Cancel' }).click();
-await page.getByRole('button', { name: 'Add Proposal' }).click();
-await page.getByRole('textbox', { name: 'Title * 0/' }).click();
-await page.getByRole('textbox', { name: 'Title * 0/' }).fill('Proposal Test 2');
-await page.getByText('Description 0/').click();
-await page.getByLabel('Description  0/').click();
-await page.getByLabel('Description  0/').fill('Proposal Test 2');
-await page.getByRole('button', { name: 'Confirm' }).click();
-await expect(page.getByText('Successfully added proposal')).toBeVisible();
-await page.getByRole('button', { name: 'Proposal Test 2 Proposal Test' }).click();
+    //Proposal phase
+    await page.getByRole('button', { name: 'Add Proposal' }).click();
+    await page.getByRole('textbox', { name: 'Title * 0/' }).click();
+    await page.getByRole('textbox', { name: 'Title * 0/' }).fill('Proposal test');
+    await page.getByLabel('Description  0/').click();
+    await page.getByLabel('Description  0/').fill('Proposal Test');
+    await page.getByRole('button', { name: 'Cancel' }).click();
+    await page.getByRole('button', { name: 'Add Proposal' }).click();
+    await page.getByRole('textbox', { name: 'Title * 0/' }).click();
+    await page.getByRole('textbox', { name: 'Title * 0/' }).fill('Proposal Test 2');
+    await page.getByText('Description 0/').click();
+    await page.getByLabel('Description  0/').click();
+    await page.getByLabel('Description  0/').fill('Proposal Test 2');
+    await page.getByRole('button', { name: 'Confirm' }).click();
+    await expect(page.getByText('Successfully added proposal')).toBeVisible();
+    await page.getByRole('button', { name: 'Proposal Test 2 Proposal Test' }).click();
+
+    await page.getByRole('textbox', { name: 'Title * 0/' }).click();
+    await page.getByRole('textbox', { name: 'Title * 0/' }).fill('Proposal Test 3');
+    await page.getByText('Description 0/').click();
+    await page.getByLabel('Description  0/').click();
+    await page.getByLabel('Description  0/').fill('Proposal Test 3');
+    await page.getByRole('button', { name: 'Confirm' }).click();
+    await expect(page.getByText('Successfully added proposal')).toBeVisible();
+    await page.getByRole('button', { name: 'Proposal Test 3 Proposal Test' }).click();
+
+    await page.locator('#poll-header-multiple-choices').first().click();
+    await page.getByRole('button', { name: 'Fast Forward' }).click();
+
+    // Prediction Statement Phase
+    await page.waitForTimeout(600);
+    await page.getByRole('button', { name: 'See More' }).nth(1).click();
+    await page.getByRole('button', { name: 'Proposal test 3 Proposal test' }).getByRole('button').first().click();
+    await page.getByRole('button', { name: 'See More' }).nth(3).click();
+    await page.getByRole('button', { name: 'Proposal Test 2 Proposal Test' }).getByRole('button').first().click();
+    await page.getByRole('button', { name: 'Create Consequence' }).click();
+    await page.getByRole('textbox', { name: 'Title * 0/' }).click();
+    await page.getByRole('textbox', { name: 'Title * 0/' }).fill('Prediction 1');
+    await page.getByLabel('Description  0/').click();
+    await page.getByLabel('Description  0/').fill('Prediction 1');
+    await page.getByPlaceholder('-12-31 23:00:00').click();
+    await page.getByRole('combobox').nth(2).selectOption('2029');
+    await page.locator('div').filter({ hasText: /^18$/ }).click();
+    await page.getByRole('button', { name: 'Submit' }).click();
+    await expect(page.getByText('Successfully created')).toBeVisible();
 });
