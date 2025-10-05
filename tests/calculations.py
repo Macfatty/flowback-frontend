3e7a9599a8c419c6ff7fc2bba38283b20cb95265
Kattenelvis <53380638+Kattenelvis@users.noreply.github.com>
2025-10-05 18:07:58 +0200

    calculations

--- tests/calculations.py
+++ tests/calculations.py
@@ -45,46 +45,104 @@ bias = np.array(bias_estimation_all(historical_bets, historical_evaluations))
 print(bias)
 
 
-# bias = (sum(B_i - x_i))/N 
-# B är bet som inte är bias adjusted
-# b_j = B_j - bias_j
-
+# bias = (sum(B_i - x_i))/N
 def bias_adjustment(bets, bias):
     adjusted_bets = []
     for i in range(len(bets)):
         adjusted_bet = bets[i] - bias[i]
-        
+
         # Make sure the adjusted bet is between 0 and 1
         adjusted_bet = max(0, min(1, adjusted_bet))
         adjusted_bets.append(adjusted_bet)
     return np.array(adjusted_bets)
 
 
+# b_j = B_j - bias_j
 # The first F_T in Grangers paper
-adjusted_current_bets = []
-for i in range(len(current_bets)):
-    adjusted_current_bets.append(bias_adjustment(current_bets[i], bias[i]))
+def bias_adjustment_all(current_bets, bias):
+    adjusted_current_bets = []
+    for i in range(len(current_bets)):
+        temp = []
+        for j in range(len(current_bets[i])):
+            temp.append(bias_adjustment(current_bets[i], bias[i]))
+
+        adjusted_current_bets.append(np.array(temp))
+
+    return adjusted_current_bets
+
+
+adjusted_current_bets = bias_adjustment_all(current_bets, bias)
+print("Adjusted current bets:", adjusted_current_bets)
 
 # The second F_T in Grangers paper
 adjusted_historical_bets = []
 for i in range(len(historical_bets)):
     adjusted_historical_bets.append(bias_adjustment(historical_bets[i], bias[i]))
 
-    
 
 print("Adjusted historical bets:", adjusted_historical_bets)
 
 
-
 # Calculate e's
+errors = []
+for i in range(len(historical_bets)):
+    errors.append(historical_evaluations[i] - adjusted_historical_bets[i])
+
+
+def drop_incomparable_values(arr_1, arr_2):
+    drop_list = []
+
+    for i in range(len(arr_1)):
+        if arr_1[i] is None or arr_2[i] is None:
+            drop_list.append(i)
+
+    arr_1 = np.delete(arr_1, drop_list)
+    arr_2 = np.delete(arr_2, drop_list)
+
+    return arr_1, arr_2
+
+
+def covariance(arr_1, arr_2):
+    covariance_array = [
+        (arr_1[i] - np.mean(arr_1)) * (arr_2[i] - np.mean(arr_2))
+        for i in range(len(arr_1))
+    ]
+    return (1 / len(arr_1)) * sum(covariance_array)
+
+
+covariance_matrix = []
+for k in range(len(errors)):
+    row = []
+
+    for j in range(len(errors)):
+        comparable_errors = drop_incomparable_values(errors[k], errors[j])
+        row.append(covariance(comparable_errors[0], comparable_errors[1]))
+
+    covariance_matrix.append(row)
+
+inverse_covariance_matrix = np.linalg.pinv(covariance_matrix)
+column_one_vector = np.ones((len(covariance_matrix), 1))
+row_one_vector = np.ones((1, len(covariance_matrix)))
+print("Covariance matrix:", covariance_matrix)
 # Calculate covariance matrix
+# Calculate per row/column, ignore the E
 
+nominator = np.matmul(inverse_covariance_matrix, column_one_vector)
+
+denominator_vector = np.matmul(inverse_covariance_matrix, row_one_vector)
+denominator = np.matmul(row_one_vector, denominator_vector)
+
+small_decimal = 10 ** -7
+if denominator == 0:
+    denominator = small_decimal
+
+bet_weights = nominator / denominator
+transposed_bet_weights = np.transpose(bet_weights)
+
+print("Transposed_bet_weights:", transposed_bet_weights)
+# print("Main bets:", main_bets)
+# print("Bias_adjustments:", bias_adjustments)
 # Calculate k's
-
-
-
-
-
 
 
 # Edgecase: All bets are the same
@@ -94,12 +152,7 @@ print("Adjusted historical bets:", adjusted_historical_bets)
 # Edgecase: Evaluation has reached 50% on the vote (in this instance it counts as not evaluated)
 
 
-
-
-
 # E(e^T e) = e^T e
+# Otherwise take mean
 
-# Otherwise take mean 
-
-#F_T are unbiased bets. Like b in Loke's article
-
+# F_T are unbiased bets. Like b in Loke's article
