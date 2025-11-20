ffd11ea49947752c247b4b63fa7038f32098d0b1
Kattenelvis <53380638+Kattenelvis@users.noreply.github.com>
2025-11-20 15:02:44 +0100

    Added todo

--- src/lib/Generic/Modal.svelte
+++ src/lib/Generic/Modal.svelte
@@ -13,6 +13,8 @@
 		buttons: ModalButton[] = [],
 		id = 'popup-modal';
 
+	let modal:HTMLDivElement | undefined;
+
 	const closeModal = (event: MouseEvent) => {
 		event.stopPropagation();
 		open = false;
@@ -31,6 +33,10 @@
 	const onOpenModal = () => {
 		hideScrollbar(true);
 		onOpen();
+		//TODO Fix focus for keyboard navigation
+		modal?.focus()
+		console.log(modal);
+		
 	};
 
 	const onCloseModal = () => {
@@ -38,26 +44,27 @@
 		onClose();
 	};
 
-	// $: if (open) onOpenModal();
+	$: if (open) onOpenModal();
 </script>
 
 <div
 	id="overlay"
-	class="overlay "
+	class="overlay"
 	class:hidden={!open}
 	on:click={closeModal}
-	tabindex="0"
+	tabindex="-1"
 	on:keydown
 	role="button"
 >
 	<div
 		{id}
-		tabindex="-1"
 		class={`w-[80%] !cursor-default max-h-[80vh] mt-10 dark:bg-darkbackground bg-white overflow-y-auto overflow-x-hidden border
 		border-gray-300 rounded shadow-xl fixed left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 z-50 max-w-[400px] ${Class}`}
 		on:click={stopPropagation}
+		tabindex="0"
 		on:keydown
 		role="button"
+		bind:this={modal}
 	>
 		<div class="dark:text-darkmodeText relative p-4 w-full h-full">
 			<!-- <div class="text-xl border-b-2 border-gray-300 border-solid  break-word"> -->
@@ -80,7 +87,12 @@
 			{/if}
 			<div class="flex justify-center gap-2">
 				{#each buttons as button}
-					<Button id={button.label} buttonStyle={button.type} Class={`flex-1 ${button.class || ''}`} onClick={button.onClick}>
+					<Button
+						id={button.label}
+						buttonStyle={button.type}
+						Class={`flex-1 ${button.class || ''}`}
+						onClick={button.onClick}
+					>
 						{$_(button.label)}
 					</Button>
 				{/each}
